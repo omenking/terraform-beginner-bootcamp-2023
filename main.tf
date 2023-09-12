8a23e81f3338961aad73e830c53603db050fd9d2
Andrew Brown <andrew@monsterboxpro.com>
2023-09-11 21:39:05 -0400

    #9 add random terraform provider (#10)

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length   = 16
+  special  = false
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
