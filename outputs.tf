f4a5a13c087aae6228120272d569b4fbfa452035
Andrew Brown <andrew@monsterboxpro.com>
2023-09-14 19:43:37 +0000

    #27 enabled static website hosting, uploaded index and error file via terraform

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+  description = "S3 Static Website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
