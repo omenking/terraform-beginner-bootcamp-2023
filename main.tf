f30f764e5db802e7aa6a0595b1e73f986d29402e
Andrew Brown <andrew@monsterboxpro.com>
2023-09-14 21:40:26 +0000

    #32 only upload index.html if a content version has change.

--- main.tf
+++ main.tf
@@ -22,4 +22,5 @@ module "terrahouse_aws" {
   bucket_name = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
 }
