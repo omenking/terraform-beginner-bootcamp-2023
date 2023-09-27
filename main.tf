72c519cb13761254845e62fc96eb8a4b8488358e
Andrew Brown <andrew@monsterboxpro.com>
2023-09-27 20:31:20 +0000

    #47 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   #backend "remote" {
   #  hostname = "app.terraform.io"
   #  organization = "ExamPro"
@@ -16,11 +22,17 @@ terraform {
 
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
+
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  index_html_filepath = var.index_html_filepath
+#  error_html_filepath = var.error_html_filepath
+#  content_version = var.content_version
+#}
