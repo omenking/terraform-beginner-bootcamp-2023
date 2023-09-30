a8e4746892fec437b361e6d3095a73c7566cec1c
Andrew Brown <andrew@monsterboxpro.com>
2023-09-30 17:42:21 +0000

    comment out more

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -20,6 +20,7 @@ variable "bucket_name" {
   }
 }
 
+/*
 variable "index_html_filepath" {
   description = "The file path for index.html"
   type        = string
@@ -39,3 +40,4 @@ variable "error_html_filepath" {
     error_message = "The provided path for error.html does not exist."
   }
 }
+*/
